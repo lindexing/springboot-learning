09944c159bf11bd2876aa8126a8065d9e24f3443
lindexing <147416297@qq.com>
2018-10-25 14:58:16 +0800

    22

--- springboot-01-helloworld/src/main/java/com/cuzz/Application.java
+++ springboot-01-helloworld/src/main/java/com/cuzz/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author: cuzz
+ * @Author: 22
  * @Date: 2018/9/20 18:06
  * @Description: @SpringBootApplication 来标注一个主程序，说明这是一个SpringBoot应用
  */
